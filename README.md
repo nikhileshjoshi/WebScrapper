f3ccc1f0f7120e62b86b19c38a4f3daa09880433
Nikhilesh Shankar <reachnikhilesh@gmail.com>
2017-03-16 22:00:35 -0400

    Update README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebScrapper
+
+I wrote this small program for a way to understand GoQuery and pull data from a table in a web page. This based on an article written on scrapping data from a web page using python.asdfasdf
+
+http://blog.kaggle.com/2017/01/31/scraping-for-craft-beers-a-dataset-creation-tutorial/asdfasdfasdf
+
+So I wrote a small web scrapper in Go.
--- WebScrapper.go
+++ WebScrapper.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
-	"net/http"
+	"time"
 )
 
 type beerType struct {
@@ -19,44 +19,26 @@ type beerType struct {
 }
 
 func main() {
-	resp, err := http.Get("http://craftcans.com/db.php?search=all&sort=beerid&ord=desc&view=text")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	b := resp.Body
-	defer b.Close()
+	t := time.Now()
 
 	doc, err := goquery.NewDocument("http://craftcans.com/db.php?search=all&sort=beerid&ord=desc&view=text")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//beers := make([]beer,100)
 	var beers []beerType
-	//index := 0
 
 	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
-		//fmt.Println(i, len(s.Contents().Nodes) ,s.Contents().Text())
 		tds := s.Children()
 
 		if tds.Size() == 8 {
-			//index++
 			beer := beerType{tds.Slice(0, 1).Text(), tds.Slice(1, 2).Text(), tds.Slice(2, 3).Text(), tds.Slice(3, 4).Text(),
 				tds.Slice(4, 5).Text(), tds.Slice(5, 6).Text(), tds.Slice(6, 7).Text(), tds.Slice(7, 8).Text()}
-			/*beers[index].beer_id = tds.Slice(0,1).Text()
-			beers[index].beer_name = tds.Slice(1,2).Text()
-			beers[index].brewery = tds.Slice(2,3).Text()
-			beers[index].location = tds.Slice(3,4).Text()
-			beers[index].style = tds.Slice(4,5).Text()
-			beers[index].size = tds.Slice(5,6).Text()
-			beers[index].abv = tds.Slice(6,7).Text()
-			beers[index].ibus = tds.Slice(7,8).Text()*/
 			beers = append(beers, beer)
 
 		}
 	})
 
 	fmt.Println(beers)
-
+	fmt.Println(time.Since(t))
 }
