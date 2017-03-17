ef87b215af426bec1ca1f294f60a114ed94177e1
Nikhilesh Shankar <reachnikhilesh@gmail.com>
2017-03-16 21:19:44 -0400

    Complete Pull

--- WebScrapper.go
+++ WebScrapper.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 )
 
-type beer struct {
-	beer_id   int
+type beerType struct {
+	beer_id   string
 	beer_name string
 	brewery   string
 	location  string
@@ -32,16 +32,31 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//beers := make([]beer,100)
+	var beers []beerType
+	index := 0
+
 	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
 		//fmt.Println(i, len(s.Contents().Nodes) ,s.Contents().Text())
 		tds := s.Children()
-		if s.Contents().Size() == 8 {
-			fmt.Println(tds.Slice(0,1).Text(), "--" ,tds.Slice(1,2).Text(),tds.Slice(2,3).Text(),tds.Slice(3,4).Text(),
-													tds.Slice(4,5).Text(),tds.Slice(5,6).Text(),tds.Slice(6,7).Text(),tds.Slice(7,8).Text())
+
+		if tds.Size() == 8 {
+			index++
+			beer := beerType{tds.Slice(0, 1).Text(), tds.Slice(1, 2).Text(), tds.Slice(2, 3).Text(), tds.Slice(3, 4).Text(),
+				tds.Slice(4, 5).Text(), tds.Slice(5, 6).Text(), tds.Slice(6, 7).Text(), tds.Slice(7, 8).Text()}
+			/*beers[index].beer_id = tds.Slice(0,1).Text()
+			beers[index].beer_name = tds.Slice(1,2).Text()
+			beers[index].brewery = tds.Slice(2,3).Text()
+			beers[index].location = tds.Slice(3,4).Text()
+			beers[index].style = tds.Slice(4,5).Text()
+			beers[index].size = tds.Slice(5,6).Text()
+			beers[index].abv = tds.Slice(6,7).Text()
+			beers[index].ibus = tds.Slice(7,8).Text()*/
+			beers = append(beers, beer)
+
 		}
-		/*s.First().Find("td").Each(func(i int, s *goquery.Selection){
-			fmt.
-		})*/
 	})
+
+	fmt.Println(beers)
 
 }
